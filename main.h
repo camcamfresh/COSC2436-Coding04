e767cd2533bbd42664fdc8bacbf9b24fa22ec132
Cameron <Desktop@Desktop.com>
2019-06-26 01:19:51 -0500

    added headers

--- main.h
+++ main.h
@@ -1,3 +1,10 @@
+/* Author: Cameron Salazar
+ * Assignment: coding04 - Hash Tables
+ * Data: 6/26/2019
+ * Purpose: The purpose of this program is to test the implementation of a hash table using a linked list.
+ * 			The linked list is the same one from coding03 with slight modifications, main.ino preforms multiple
+ *			test on the hasttable class located in hashtable.cpp.
+*/
 #ifndef HASH_TABLE_MAIN_H
 #define HASH_TABLE_MAIN_H
 
